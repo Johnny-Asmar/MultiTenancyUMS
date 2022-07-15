4187d10eb31ea07617daf2060bc1899bc211ca77
Johnny El Asmar <johnny.asmar@hotmail.com>
2022-07-15 11:06:51 +0300

    Models Ready

---
+++ Domain/Models/SessionTime.cs
@@ -0,0 +1,16 @@
+namespace Domain.Models;
+
+public partial class SessionTime
+{
+    public SessionTime()
+    {
+        TeacherPerCoursePerSessionTimes = new HashSet<TeacherPerCoursePerSessionTime>();
+    }
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public long Id { get; set; }
+    public int Duration { get; set; }
+
+    public virtual ICollection<TeacherPerCoursePerSessionTime> TeacherPerCoursePerSessionTimes { get; set; }
+}
